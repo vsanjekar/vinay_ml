8c79466a5c94881b97d4f81d9bf9e97eeb6d73b2
Vinay Sanjekar <vinay@betatrek.com>
2014-07-27 22:13:50 +0000

    Regression Analysis test code

---
+++ regresion-analysis/test.py
@@ -0,0 +1,6 @@
+from scipy import stats
+import numpy as np
+x = np.random.random(10)
+y = np.random.random(10)
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print "r-squared:", r_value**2
